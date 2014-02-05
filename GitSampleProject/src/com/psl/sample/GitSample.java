918e061b6b47dc2851ae4edba56b105c5cf0b128
sunetra <sunetra_pandit@persistent.co.in>
2014-02-05 17:54:07 +0530

    please work

--- GitSampleProject/src/com/psl/sample/GitSample.java
+++ GitSampleProject/src/com/psl/sample/GitSample.java
@@ -5,8 +5,7 @@ public class GitSample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git test project for demo");
-		System.out.println("Changes by sunetra");
-			System.out.println("Changes by sunetra");
+		System.out.println("Changes by sunetra");		
 	}
 
 }
