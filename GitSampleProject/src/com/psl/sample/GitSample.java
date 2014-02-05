a6c98c3c50326bfedfd42d7394cc470d651c9c76
sunetra <sunetra_pandit@persistent.co.in>
2014-02-05 16:36:03 +0530

    initial commit

---
+++ GitSampleProject/src/com/psl/sample/GitSample.java
@@ -0,0 +1,10 @@
+package com.psl.sample;
+
+public class GitSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git test project for demo");
+	}
+
+}
