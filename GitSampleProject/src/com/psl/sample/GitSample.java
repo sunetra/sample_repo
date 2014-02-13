d83a56c8601022467f28a6db972f6b57d88e1728
sunetra <sunetra_pandit@persistent.co.in>
2014-02-13 09:10:07 +0530

    updated date

--- GitSampleProject/src/com/psl/sample/GitSample.java
+++ GitSampleProject/src/com/psl/sample/GitSample.java
@@ -7,5 +7,6 @@ public class GitSample {
 		System.out.println("Git test project for demo");
 		System.out.println("Changes by sunetra");	
 		System.out.println("Change in other");
+		System.out.println("Changed on 13th Feb");
 	}
 }
